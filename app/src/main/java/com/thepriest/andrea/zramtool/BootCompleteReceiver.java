e0897ac8cac38d22db0596ea9983a3dacefcbc5f
MarioUnoDueTreQuattro <andreagi0@yahoo.it>
2015-06-12 16:11:25 +0200

    On_BootCompleted

--- app/src/main/java/com/thepriest/andrea/zramtool/BootCompleteReceiver.java
+++ app/src/main/java/com/thepriest/andrea/zramtool/BootCompleteReceiver.java
@@ -4,9 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.util.Log;
-
 
 public class BootCompleteReceiver extends BroadcastReceiver {
     //public static final String PREFS_NAME = "SharedPreferences.xml";
