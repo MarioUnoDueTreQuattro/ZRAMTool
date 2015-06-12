b1bcef8037ca47d7f32def1a6fcefcc8421ea02a
MarioUnoDueTreQuattro <andreagi0@yahoo.it>
2015-06-12 07:31:50 +0200

    On_BootCompleted

---
+++ app/src/main/java/com/thepriest/andrea/zramtool/BootCompleteReceiver.java
@@ -0,0 +1,37 @@
+package com.thepriest.andrea.zramtool;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+    public static final String PREFS_NAME = "MyPrefsFile";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            Log.d("boot completed", "boot completed caught");
+
+
+            Boolean autoRestart = false;
+            SharedPreferences sp = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+            autoRestart = sp.getBoolean("OnBootCompleted_AutoRestartIfNotificationIsEnabled", false);
+
+            if (autoRestart) {
+
+                Log.d("boot completed", "auto restart true");
+
+                Intent i = new Intent(context, NotificationService.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+
+            }
+        }
+
+
+    }
+}
