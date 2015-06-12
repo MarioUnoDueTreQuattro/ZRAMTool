c18026325072c9ad9d038e8c4c9c9ae93a54278f
MarioUnoDueTreQuattro <andreagi0@yahoo.it>
2015-06-12 07:49:06 +0200

    On_BootCompleted

--- app/src/main/java/com/thepriest/andrea/zramtool/BootCompleteReceiver.java
+++ app/src/main/java/com/thepriest/andrea/zramtool/BootCompleteReceiver.java
@@ -4,28 +4,28 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 
 public class BootCompleteReceiver extends BroadcastReceiver {
-    public static final String PREFS_NAME = "MyPrefsFile";
+    //public static final String PREFS_NAME = "SharedPreferences.xml";
+    public static final String PREFS_NAME = "com.thepriest.andrea.zramtool";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO Auto-generated method stub
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            Log.d("boot completed", "boot completed caught");
-
-
+            Log.d("boot completed", "boot completed caught");;
             Boolean autoRestart = false;
             SharedPreferences sp = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-            autoRestart = sp.getBoolean("OnBootCompleted_AutoRestartIfNotificationIsEnabled", false);
+            autoRestart = sp.getBoolean("enable_notification", false);
 
             if (autoRestart) {
 
                 Log.d("boot completed", "auto restart true");
 
-                Intent i = new Intent(context, NotificationService.class);
+                Intent i = new Intent(context, MainActivity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(i);
 
