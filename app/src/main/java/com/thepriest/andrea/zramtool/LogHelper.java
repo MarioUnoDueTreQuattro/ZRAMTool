53fbbf36756c953affa7440cbd39147d225b768e
MarioUnoDueTreQuattro <andreagi0@yahoo.it>
2015-06-16 16:47:40 +0200

    LogHelper class

---
+++ app/src/main/java/com/thepriest/andrea/zramtool/LogHelper.java
@@ -0,0 +1,12 @@
+package com.thepriest.andrea.zramtool;
+
+public class LogHelper {
+
+    public enum LogColor {
+        WHITE, RED, GREEN, BLUE, GRAY
+    }
+
+    private static final String TAG = LogHelper.class.getSimpleName();
+
+
+}
